8ab17656b784f58f53df1634c4aac44931dd39dc
Aaron Robson <ARobsonSE@hotmail.co.uk>
2018-01-02 22:44:38 +0000

    Included chords.

--- odetojoy.py
+++ odetojoy.py
@@ -2,15 +2,18 @@ from mido import Message, MidiFile, MidiTrack
 
 from noteconv import to_note
 
-mid = MidiFile()
-track = MidiTrack()
-mid.tracks.append(track)
+mid = MidiFile(type=1)
+main_track = MidiTrack()
+chord_track = MidiTrack()
+mid.tracks.append(main_track)
+mid.tracks.append(chord_track)
 
-track.append(Message('program_change', program=12, time=0))
+main_track.append(Message('program_change', program=12, time=0))
+chord_track.append(Message('program_change', program=12, time=0))
 
 beat_length = 200
 
-def do_note(note, beats):
+def do_note(track, note, beats):
     time = int(beats*beat_length)
     if note is None:
         #rest
@@ -19,7 +22,7 @@ def do_note(note, beats):
         track.append(Message('note_on', note=note, velocity=127, time=0))
         track.append(Message('note_off', note=note, velocity=127, time=time))
 
-notes = [
+main_notes = [
     #1st line
 
     (to_note('e'), 1),
@@ -106,7 +109,37 @@ notes = [
     (to_note('c'), 0.5),
     (to_note('c'), 2),
 ]
-for note, beats in notes:
-    do_note(note, beats)
+for note, beats in main_notes:
+    do_note(main_track, note, beats)
+
+chord_notes = [
+    #1st line
+    (to_note('c', 4), 4),
+    (to_note('g', 3), 4),
+    (to_note('c', 4), 4),
+    (to_note('g', 3), 4),
+ 
+    #2nd line
+    (to_note('c', 4), 4),
+    (to_note('g', 3), 4),
+    (to_note('c', 4), 4),
+    (to_note('g', 3), 2),
+    (to_note('c', 4), 2),
+
+    #3rd line
+    (to_note('g', 3), 4),
+    (to_note('g', 3), 4),
+    (to_note('g', 3), 4),
+    (to_note('g', 3), 4),
+
+    #4th line
+    (to_note('c', 4), 4),
+    (to_note('g', 3), 4),
+    (to_note('c', 4), 4),
+    (to_note('g', 3), 2),
+    (to_note('c', 4), 2),
+]
+for note, beats in chord_notes:
+    do_note(chord_track, note, beats)
 
 mid.save('odetojoy.midi')
